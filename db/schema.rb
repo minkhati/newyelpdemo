43d224d3fc7c35f8481ddae39aa0a52b7ce7c27d
Min Khati <minkhati@cdcsit.edu.np>
2015-02-20 03:55:07 +0000

    Link Reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220020213) do
+ActiveRecord::Schema.define(version: 20150220034030) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150220020213) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
