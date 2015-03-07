feb1c69ec9cc443d083d08087014f35ad0ec4025
Min Khati <minkhati@cdcsit.edu.np>
2015-03-07 23:51:03 +0000

    Remove Show and Index URLs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,21 +12,42 @@ class ReviewsController < ApplicationController
   def edit
   end
 
+  # POST /reviews
+  # POST /reviews.json
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    @review.save
-    respond_with(@review)
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to root_path, notice: 'Review was successfully created.'}
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
+  # PATCH/PUT /reviews/1
+  # PATCH/PUT /reviews/1.json
   def update
-    @review.update(review_params)
-    respond_with(@review)
+     respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Review was successfully updated.'}
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
