92345c253e7a613f3b4b032a3465db1f9a446a42
Min Khati <minkhati@cdcsit.edu.np>
2015-03-08 00:15:33 +0000

    Link Reviews with Restaurant

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_restaurant
   before_action :authenticate_user!
 
   respond_to :html
@@ -17,6 +18,8 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.restaurant_id = @restaurant.id
+    
     respond_to do |format|
       if @review.save
         format.html { redirect_to root_path, notice: 'Review was successfully created.'}
@@ -55,6 +58,10 @@ class ReviewsController < ApplicationController
       @review = Review.find(params[:id])
     end
 
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+    
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
